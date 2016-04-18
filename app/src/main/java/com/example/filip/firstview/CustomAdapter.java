24dc15ce52e88f0248e928e42cbe08329313b12e
Filip <filiplama@gmail.com>
2016-04-18 14:05:26 +0200

    minor edits loading dialog added

--- app/src/main/java/com/example/filip/firstview/CustomAdapter.java
+++ app/src/main/java/com/example/filip/firstview/CustomAdapter.java
@@ -1,6 +1,7 @@
 package com.example.filip.firstview;
 
 import android.content.Context;
+import android.support.v7.widget.RecyclerView;
 import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
@@ -17,7 +18,7 @@ import java.util.List;
  * Created by filip on 09/04/2016.
  */
 
-class CustomAdapter extends ArrayAdapter<Task> {
+class CustomAdapter extends RecyclerView.Adapter<Task> {
 
 
     public CustomAdapter(Context context, List<Task> taskData) {
@@ -69,5 +70,20 @@ class CustomAdapter extends ArrayAdapter<Task> {
         });
 
         return customView;
+    }
+
+    @Override
+    public Task onCreateViewHolder(ViewGroup parent, int viewType) {
+        return null;
+    }
+
+    @Override
+    public void onBindViewHolder(Task holder, int position) {
+
+    }
+
+    @Override
+    public int getItemCount() {
+        return 0;
     }
 }

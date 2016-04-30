35146b940e5beb2a3a6bed1a687544246d0ed413
Filip <filiplama@gmail.com>
2016-04-30 18:54:08 +0200

    feedback fix

--- app/src/main/java/com/zafodB/PhoToDo/Feedback.java
+++ app/src/main/java/com/zafodB/PhoToDo/Feedback.java
@@ -1,17 +1,43 @@
 package com.zafodB.PhoToDo;
 
 import android.os.Bundle;
+import android.os.SystemClock;
+import android.provider.*;
+import android.provider.Settings;
 import android.support.v7.app.AppCompatActivity;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.TextView;
+
+import com.firebase.client.Firebase;
 
 /**
  * Created by filip on 07/04/2016.
  */
 public class Feedback extends AppCompatActivity {
+    Firebase localRef;
 
+    EditText feedbackText;
+    Button sendFeedback;
+
+    //Sets up feedback activity with simple method sending feedback.
     @Override
     protected void onCreate(Bundle savedInstanceState) {
-
         super.onCreate(savedInstanceState);
         setContentView(R.layout.send_feedback_activity);
+
+        localRef = ApplicationMain.getFirebaseRef();
+
+        feedbackText = (EditText)findViewById(R.id.feedback_edit);
+
+        sendFeedback = (Button) findViewById(R.id.send_feedback);
+        sendFeedback.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                localRef.child("Feedback").child(ApplicationMain.getUserAuthData().getUid() + " " + String.valueOf
+                        (SystemClock.currentThreadTimeMillis())).setValue(feedbackText.getText().toString());
+            }
+        });
     }
 }
